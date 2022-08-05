02464572743f49f558ca03057ffcd33bd3d6e0a6
yagoalessio88 <yagoalessio88@gmail.com>
2022-08-05 21:03:27 +0200

    checking verbs conjugation answers functionality

--- src/components/Verbe.js
+++ src/components/Verbe.js
@@ -1,31 +1,64 @@
 import styled from "styled-components";
 import { data } from "../data/conjugaison-data.js";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const Verbe = () => {
   const { id } = useParams();
   const verbo = data.filter((element) => element.nombre === id)[0];
   const { conjugaison } = verbo;
 
+  const checkAnswers = () => {
+    let ul = document.querySelector(".list-conjugaison");
+    const listElements = [...ul.querySelectorAll(".text-input")];
+    const listUserValues = listElements.map((item) => {
+      return item.value;
+    });
+    const answers = conjugaison.map((item) => {
+      return item.reponse;
+    });
+
+    answers.forEach((item, index) => {
+      if (listUserValues[index] !== "") {
+        if (item.toUpperCase() === listUserValues[index].toUpperCase()) {
+          const element = document.getElementById(index);
+          element.classList.remove("wrong_answer");
+        } else {
+          const element = document.getElementById(index);
+          console.log(element);
+          element.classList.add("wrong_answer");
+        }
+      } else {
+        const element = document.getElementById(index);
+        console.log(element);
+        element.classList.add("wrong_answer");
+      }
+    });
+  };
+
   return (
     <Wrapper>
       <article className="main-article main-verbe">
         <h3 className="main-verbe__title">{id}</h3>
-        <ul>
-          {conjugaison.map((e, index) => {
-            const { first, last } = e;
+
+        <ul className="list-conjugaison">
+          {conjugaison.map((item, index) => {
+            const { first, last } = item;
             return (
-              <li key={index} className="main-verbe__li-element">
+              <li key={index}>
                 {first}
-                <input type="text" />
+                <input className="text-input" id={index} type="text" />
                 {last}
               </li>
             );
           })}
         </ul>
-
-        <button type="button" className="button main-verbe__button">
-          voir conjugaison
+        <button
+          type="button"
+          className="button main-verbe__button"
+          onClick={checkAnswers}
+        >
+          Vérifier les Réponses
         </button>
       </article>
     </Wrapper>
@@ -34,4 +67,8 @@ const Verbe = () => {
 
 export { Verbe };
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  .wrong_answer {
+    border: 1px solid red;
+  }
+`;
--- src/data/conjugaison-data.js
+++ src/data/conjugaison-data.js
@@ -2,35 +2,92 @@ export const data = [
   {
     nombre: "aller",
     conjugaison: [
-      { first: "Je", last: "au marché, acheter des légumes." },
-      { first: "Tu", last: "chez le médecin?" },
-      { first: "On", last: "au cinéma demain." },
-      { first: "Julien", last: "à la mer avec ses parents." },
-      { first: "Nous", last: "dans les bois, faire du vélo." },
-      { first: "", last: "vous à Paris fréquemment?" },
-      { first: "Aujourd'hui, Lea et Marie", last: "à l'ecole." },
+      { first: "Je", reponse: "vais", last: "au marché, acheter des légumes." },
+      { first: "Tu", reponse: "vas", last: "chez le médecin?" },
+      { first: "On", reponse: "va", last: "au cinéma demain." },
+      {
+        first: "Julien",
+        reponse: "va",
+        last: "à la mer avec ses parents.",
+      },
+      {
+        first: "Nous",
+        reponse: "allons",
+        last: "dans les bois, faire du vélo.",
+      },
+      { first: "", reponse: "Allez", last: "vous à Paris fréquemment?" },
+      {
+        first: "Aujourd'hui, Lea et Marie",
+        reponse: "vont",
+        last: "à l'ecole.",
+      },
     ],
   },
   {
     nombre: "avoir",
     conjugaison: [
-      { first: "Aujourd'hui j'", last: "mal à la tête toute la journée." },
-      { first: "Tu", last: "vu ce film?" },
-      { first: "Elle avait beaucoup de poupées lorsqu'elle", last: "10 ans." },
-      { first: "On", last: "cherché partout." },
+      {
+        first: "Aujourd'hui j'",
+        reponse: "ai",
+        last: "mal à la tête toute la journée.",
+      },
+      { first: "Tu", reponse: "as", last: "vu ce film?" },
+      {
+        first: "Elle avait beaucoup de poupées lorsqu'elle",
+        reponse: "a",
+        last: "10 ans.",
+      },
+      { first: "On", reponse: "a", last: "cherché partout." },
       {
         first: "Malgré tous nos problèmes que nous",
+        reponse: "avons",
         last: "jusqu'ici, nous avons l'opportunité de partir en vacances.",
       },
       {
         first: "Il faudrait que vous",
+        reponse: "avez",
         last: "beaucoup de chance pour réussir votre examen.",
       },
 
       {
         first: "Bien qu'elles",
+        reponse: "ont",
         last: "une augmentation, leurs salaires étaient encore trop bas.",
       },
     ],
   },
 ];
+
+//  {
+//     nombre: "",
+//     conjugaison: [
+//       {
+//         first: "",
+//         reponse: "a",
+//         last: "",
+//       },
+//       { first: "", reponse: "", last: "" },
+//       {
+//         first: "",
+//         reponse: "",
+//         last: "",
+//       },
+//       { first: "", reponse: "", last: "" },
+//       {
+//         first: "",
+//         reponse: "",
+//         last: "",
+//       },
+//       {
+//         first: "",
+//         reponse: "",
+//         last: "",
+//       },
+
+//       {
+//         first: "",
+//         reponse: "",
+//         last: "",
+//       },
+//     ],
+//   },
