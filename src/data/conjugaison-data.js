d3c98784631058a3b5c7db9bb350f8de5b60d2c1
yago <yagoalessio88@gmail.com>
2022-09-05 22:31:33 +0200

    data update

--- src/data/conjugaison-data.js
+++ src/data/conjugaison-data.js
@@ -1,92 +1,269 @@
 export const data = [
 	{
-		nombre: "Arriver",
+		nombre: "Atteindre",
 		conjugaison: [
 			{
 				first: "J'",
-				reponse: "arrive",
+				reponse: "atteins",
 				last: "",
-				voirConjugaison: "J'arrive",
+				voirConjugaison: "J'atteins",
 			},
 			{
 				first: "Tu",
-				reponse: "arrives",
+				reponse: "atteins",
 				last: "",
-				voirConjugaison: "Tu arrives",
+				voirConjugaison: "Tu atteins",
 			},
 			{
 				first: "Il",
-				reponse: "arrive",
+				reponse: "atteint",
 				last: "",
-				voirConjugaison: "Il/Elle arrive",
+				voirConjugaison: "Il/Elle atteint",
 			},
 			{
 				first: "On",
-				reponse: "arrive",
+				reponse: "atteint",
 				last: "",
-				voirConjugaison: "On arrive",
+				voirConjugaison: "On atteint",
 			},
 			{
 				first: "Nous",
-				reponse: "arrivons",
+				reponse: "atteignons",
 				last: "",
-				voirConjugaison: "Nous arrivons",
+				voirConjugaison: "Nous atteignons",
 			},
 			{
 				first: "Vous",
-				reponse: "arrivez",
+				reponse: "atteignez",
 				last: "",
-				voirConjugaison: "Vous arrivez",
+				voirConjugaison: "Vous atteignez",
 			},
 
 			{
 				first: "Ils",
-				reponse: "arrivent",
+				reponse: "atteignent",
 				last: "",
-				voirConjugaison: "Ils/Elles arrivent",
+				voirConjugaison: "Ils/Elles atteignent",
 			},
 		],
 	},
 	{
-		nombre: "Être",
+		nombre: "Activer",
 		conjugaison: [
 			{
-				first: "Je",
-				reponse: "suis",
-				last: "français.",
-				voirConjugaison: "Je suis",
+				first: "J'",
+				reponse: "active",
+				last: "",
+				voirConjugaison: "J'active",
 			},
 			{
 				first: "Tu",
-				reponse: "es",
-				last: "médecin.",
-				voirConjugaison: "Tu es",
+				reponse: "actives",
+				last: "",
+				voirConjugaison: "Tu actives",
 			},
 			{
 				first: "Il",
-				reponse: "est",
-				last: "heureux.",
-				voirConjugaison: "Il/Elle est",
+				reponse: "active",
+				last: "",
+				voirConjugaison: "Il/Elle active",
 			},
-			{ first: "On", reponse: "est", last: "prêt!", voirConjugaison: "On est" },
+			{
+				first: "On",
+				reponse: "active",
+				last: "",
+				voirConjugaison: "On active",
+			},
 			{
 				first: "Nous",
-				reponse: "sommes",
-				last: "mariés.",
-				voirConjugaison: "Nous sommes",
+				reponse: "activons",
+				last: "",
+				voirConjugaison: "Nous activons",
 			},
 			{
 				first: "Vous",
-				reponse: "êtes",
-				last: "grand.",
-				voirConjugaison: "Vous êtes",
+				reponse: "activez",
+				last: "",
+				voirConjugaison: "Vous activez",
 			},
 
 			{
 				first: "Ils",
-				reponse: "sont",
-				last: "deux.",
-				voirConjugaison: "Ils/Elles sont",
+				reponse: "activent",
+				last: "",
+				voirConjugaison: "Ils/Elles activent",
+			},
+		],
+	},
+	{
+		nombre: "Allumer",
+		conjugaison: [
+			{
+				first: "J'",
+				reponse: "allume",
+				last: "",
+				voirConjugaison: "J'allume",
+			},
+			{
+				first: "Tu",
+				reponse: "allumes",
+				last: "",
+				voirConjugaison: "Tu allumes",
+			},
+			{
+				first: "Il",
+				reponse: "allume",
+				last: "",
+				voirConjugaison: "Il/Elle allume",
+			},
+			{
+				first: "On",
+				reponse: "allume",
+				last: "",
+				voirConjugaison: "On allume",
+			},
+			{
+				first: "Nous",
+				reponse: "allumons",
+				last: "",
+				voirConjugaison: "Nous allumons",
+			},
+			{
+				first: "Vous",
+				reponse: "allumez",
+				last: "",
+				voirConjugaison: "Vous allumez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "allument",
+				last: "",
+				voirConjugaison: "Ils/Elles allument",
+			},
+		],
+	},
+	{
+		nombre: "Aider",
+		conjugaison: [
+			{
+				first: "J'",
+				reponse: "aide",
+				last: "",
+				voirConjugaison: "J'aide",
+			},
+			{ first: "Tu", reponse: "aides", last: "", voirConjugaison: "Tu aides" },
+			{
+				first: "Il",
+				reponse: "aide",
+				last: "",
+				voirConjugaison: "Il/Elle aide",
+			},
+			{ first: "On", reponse: "aide", last: "", voirConjugaison: "On aide" },
+			{
+				first: "Nous",
+				reponse: "aidons",
+				last: "",
+				voirConjugaison: "Nous aidons",
+			},
+			{
+				first: "Vous",
+				reponse: "aidez",
+				last: "",
+				voirConjugaison: "Vous aidez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "aident",
+				last: "",
+				voirConjugaison: "Ils/Elles aident",
+			},
+		],
+	},
+	{
+		nombre: "Aimer",
+		conjugaison: [
+			{
+				first: "J'",
+				reponse: "aime",
+				last: "",
+				voirConjugaison: "J'aime",
+			},
+			{ first: "Tu", reponse: "aimes", last: "", voirConjugaison: "Tu aimes" },
+			{
+				first: "Il",
+				reponse: "aime",
+				last: "",
+				voirConjugaison: "Il/Elle aime",
+			},
+			{ first: "On", reponse: "aime", last: "", voirConjugaison: "On aime" },
+			{
+				first: "Nous",
+				reponse: "aimons",
+				last: "",
+				voirConjugaison: "Nous aimons",
+			},
+			{
+				first: "Vous",
+				reponse: "aimez",
+				last: "",
+				voirConjugaison: "Vous aimez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "aiment",
+				last: "",
+				voirConjugaison: "Ils/Elles aiment",
+			},
+		],
+	},
+	{
+		nombre: "Arrêter",
+		conjugaison: [
+			{
+				first: "J'",
+				reponse: "arrête",
+				last: "",
+				voirConjugaison: "Je arrête",
+			},
+			{
+				first: "Tu",
+				reponse: "arrêtes",
+				last: "",
+				voirConjugaison: "Tu arrêtes",
+			},
+			{
+				first: "Il",
+				reponse: "arrête",
+				last: "",
+				voirConjugaison: "Il/Elle arrête",
+			},
+			{
+				first: "On",
+				reponse: "arrête",
+				last: "",
+				voirConjugaison: "On arrête",
+			},
+			{
+				first: "Nous",
+				reponse: "arrêtons",
+				last: "",
+				voirConjugaison: "Nous arrêtons",
+			},
+			{
+				first: "Vous",
+				reponse: "arrêtez",
+				last: "",
+				voirConjugaison: "Vous arrêtez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "arrêtent",
+				last: "",
+				voirConjugaison: "Ils/Elles arrêtent",
 			},
 		],
 	},
@@ -185,54 +362,7 @@ export const data = [
 			},
 		],
 	},
-	{
-		nombre: "S'appeler",
-		conjugaison: [
-			{
-				first: "Je",
-				reponse: "m'appelle",
-				last: "Martin.",
-				voirConjugaison: "Je m'appelle",
-			},
-			{
-				first: "¿Tu",
-				reponse: "t'appelles",
-				last: "comment?",
-				voirConjugaison: "Tu t'appelles",
-			},
-			{
-				first: "Il",
-				reponse: "s'appelle",
-				last: "Bernard.",
-				voirConjugaison: "Il/Elle s'appelle",
-			},
-			{
-				first: "On",
-				reponse: "s'appelle",
-				last: '"Encadrements Bienert".',
-				voirConjugaison: "On s'appelle",
-			},
-			{
-				first: "Nous",
-				reponse: "nous appelons",
-				last: '"Les gars de Nantes".',
-				voirConjugaison: "Nous nous appelons",
-			},
-			{
-				first: "¿Vous",
-				reponse: "vous appelez",
-				last: '"Moignard"?',
-				voirConjugaison: "Vous vous appelez",
-			},
 
-			{
-				first: "Elles",
-				reponse: "s'appellent",
-				last: "Eva et Marie.",
-				voirConjugaison: "Ils/Elles s'appellent",
-			},
-		],
-	},
 	{
 		nombre: "Appeller",
 		conjugaison: [
@@ -282,50 +412,285 @@ export const data = [
 		],
 	},
 	{
-		nombre: "Faire",
+		nombre: "Attendre",
 		conjugaison: [
 			{
-				first: "Voilà pourquoi je",
-				reponse: "fais",
-				last: "toujours les plans.",
-				voirConjugaison: "Je fais",
+				first: "J'",
+				reponse: "attends",
+				last: "",
+				voirConjugaison: "J'attends",
 			},
 			{
-				first: "J'aime comme tu",
-				reponse: "fais",
-				last: "les choses.",
-				voirConjugaison: "Tu fais",
+				first: "Tu",
+				reponse: "attends",
+				last: "",
+				voirConjugaison: "Tu attends",
 			},
 			{
-				first: "¿Comment il",
-				reponse: "fait",
-				last: "pour trouver un acheteur?",
-				voirConjugaison: "Il/Elle fait",
+				first: "Il",
+				reponse: "attend",
+				last: "",
+				voirConjugaison: "Il/Elle attend",
 			},
 			{
-				first: "Je ne suis pas totalement sûr de ce qu'on",
-				reponse: "fait",
-				last: "ici.",
-				voirConjugaison: "On fait",
+				first: "On",
+				reponse: "attend",
+				last: "",
+				voirConjugaison: "On attend",
 			},
 			{
-				first: "Dites-moi que nous",
-				reponse: "faisons",
-				last: "tout pour le récuperéer.",
-				voirConjugaison: "Nous faisons",
+				first: "Nous",
+				reponse: "attendons",
+				last: "",
+				voirConjugaison: "Nous attendons",
 			},
 			{
-				first: "Michelle, vous",
-				reponse: "faites",
-				last: "du bon travail!",
-				voirConjugaison: "Vous faites",
+				first: "Vous",
+				reponse: "attendez",
+				last: "",
+				voirConjugaison: "Vous attendez",
 			},
 
 			{
-				first: "Je ne sais pas comment elles",
-				reponse: "font",
-				last: "ça.",
-				voirConjugaison: "Ils/Elles font",
+				first: "Ils",
+				reponse: "attendent",
+				last: "",
+				voirConjugaison: "Ils/Elles attendent",
+			},
+		],
+	},
+	{
+		nombre: "Accéder",
+		conjugaison: [
+			{
+				first: "J'",
+				reponse: "accède",
+				last: "",
+				voirConjugaison: "J'accède",
+			},
+			{
+				first: "Tu",
+				reponse: "accèdes",
+				last: "",
+				voirConjugaison: "Tu accèdes",
+			},
+			{
+				first: "Il",
+				reponse: "accède",
+				last: "",
+				voirConjugaison: "Il/Elle accède",
+			},
+			{
+				first: "On",
+				reponse: "accède",
+				last: "",
+				voirConjugaison: "On accède",
+			},
+			{
+				first: "Nous",
+				reponse: "accédons",
+				last: "",
+				voirConjugaison: "Nous accédons",
+			},
+			{
+				first: "Vous",
+				reponse: "accédez",
+				last: "",
+				voirConjugaison: "Vous accédez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "accèdent",
+				last: "",
+				voirConjugaison: "Ils/Elles accèdent",
+			},
+		],
+	},
+	{
+		nombre: "Acheter",
+		conjugaison: [
+			{
+				first: "J'",
+				reponse: "achète",
+				last: "",
+				voirConjugaison: "J'achète",
+			},
+			{
+				first: "Tu",
+				reponse: "achètes",
+				last: "",
+				voirConjugaison: "Tu achètes",
+			},
+			{
+				first: "Il",
+				reponse: "achète",
+				last: "",
+				voirConjugaison: "Il/Elle achète",
+			},
+			{
+				first: "On",
+				reponse: "achète",
+				last: "",
+				voirConjugaison: "On achète",
+			},
+			{
+				first: "Nous",
+				reponse: "achetons",
+				last: "",
+				voirConjugaison: "Nous achetons",
+			},
+			{
+				first: "Vous",
+				reponse: "achetez",
+				last: "",
+				voirConjugaison: "Vous achetez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "achètent",
+				last: "",
+				voirConjugaison: "Ils/Elles achètent",
+			},
+		],
+	},
+	{
+		nombre: "Apprendre",
+		conjugaison: [
+			{
+				first: "J'",
+				reponse: "apprendrs",
+				last: "",
+				voirConjugaison: "J'apprends",
+			},
+			{
+				first: "Tu",
+				reponse: "apprends",
+				last: "",
+				voirConjugaison: "Tu apprends",
+			},
+			{
+				first: "Il",
+				reponse: "apprend",
+				last: "",
+				voirConjugaison: "Il/Elle apprend",
+			},
+			{
+				first: "On",
+				reponse: "apprend",
+				last: "",
+				voirConjugaison: "On apprend",
+			},
+			{
+				first: "Nous",
+				reponse: "apprenons",
+				last: "",
+				voirConjugaison: "Nous apprenons",
+			},
+			{
+				first: "Vous",
+				reponse: "apprenez",
+				last: "",
+				voirConjugaison: "Vous apprenez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "apprennent",
+				last: "",
+				voirConjugaison: "Ils/Elles apprennent",
+			},
+		],
+	},
+	{
+		nombre: "Arriver",
+		conjugaison: [
+			{
+				first: "J'",
+				reponse: "arrive",
+				last: "",
+				voirConjugaison: "J'arrive",
+			},
+			{
+				first: "Tu",
+				reponse: "arrives",
+				last: "",
+				voirConjugaison: "Tu arrives",
+			},
+			{
+				first: "Il",
+				reponse: "arrive",
+				last: "",
+				voirConjugaison: "Il/Elle arrive",
+			},
+			{
+				first: "On",
+				reponse: "arrive",
+				last: "",
+				voirConjugaison: "On arrive",
+			},
+			{
+				first: "Nous",
+				reponse: "arrivons",
+				last: "",
+				voirConjugaison: "Nous arrivons",
+			},
+			{
+				first: "Vous",
+				reponse: "arrivez",
+				last: "",
+				voirConjugaison: "Vous arrivez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "arrivent",
+				last: "",
+				voirConjugaison: "Ils/Elles arrivent",
+			},
+		],
+	},
+	{
+		nombre: "Bosser",
+		conjugaison: [
+			{
+				first: "Je",
+				reponse: "bosse",
+				last: "",
+				voirConjugaison: "Je bosse",
+			},
+			{
+				first: "Tu",
+				reponse: "bosses",
+				last: "",
+				voirConjugaison: "Tu bosses",
+			},
+			{
+				first: "Il",
+				reponse: "bosse",
+				last: "",
+				voirConjugaison: "Il/Elle bosse",
+			},
+			{ first: "On", reponse: "bosse", last: "", voirConjugaison: "On bosse" },
+			{
+				first: "Nous",
+				reponse: "bossons",
+				last: "",
+				voirConjugaison: "Nous bossons",
+			},
+			{
+				first: "Vous",
+				reponse: "bossez",
+				last: "",
+				voirConjugaison: "Vous bossez",
+			},
+
+			{
+				first: "Ils",
+				reponse: "bossent",
+				last: "",
+				voirConjugaison: "Ils/Elles bossent",
 			},
 		],
 	},
@@ -426,6 +791,195 @@ export const data = [
 		],
 	},
 	{
+		nombre: "Être",
+		conjugaison: [
+			{
+				first: "Je",
+				reponse: "suis",
+				last: "français.",
+				voirConjugaison: "Je suis",
+			},
+			{
+				first: "Tu",
+				reponse: "es",
+				last: "médecin.",
+				voirConjugaison: "Tu es",
+			},
+			{
+				first: "Il",
+				reponse: "est",
+				last: "heureux.",
+				voirConjugaison: "Il/Elle est",
+			},
+			{ first: "On", reponse: "est", last: "prêt!", voirConjugaison: "On est" },
+			{
+				first: "Nous",
+				reponse: "sommes",
+				last: "mariés.",
+				voirConjugaison: "Nous sommes",
+			},
+			{
+				first: "Vous",
+				reponse: "êtes",
+				last: "grand.",
+				voirConjugaison: "Vous êtes",
+			},
+
+			{
+				first: "Ils",
+				reponse: "sont",
+				last: "deux.",
+				voirConjugaison: "Ils/Elles sont",
+			},
+		],
+	},
+	{
+		nombre: "Faire",
+		conjugaison: [
+			{
+				first: "Voilà pourquoi je",
+				reponse: "fais",
+				last: "toujours les plans.",
+				voirConjugaison: "Je fais",
+			},
+			{
+				first: "J'aime comme tu",
+				reponse: "fais",
+				last: "les choses.",
+				voirConjugaison: "Tu fais",
+			},
+			{
+				first: "¿Comment il",
+				reponse: "fait",
+				last: "pour trouver un acheteur?",
+				voirConjugaison: "Il/Elle fait",
+			},
+			{
+				first: "Je ne suis pas totalement sûr de ce qu'on",
+				reponse: "fait",
+				last: "ici.",
+				voirConjugaison: "On fait",
+			},
+			{
+				first: "Dites-moi que nous",
+				reponse: "faisons",
+				last: "tout pour le récuperéer.",
+				voirConjugaison: "Nous faisons",
+			},
+			{
+				first: "Michelle, vous",
+				reponse: "faites",
+				last: "du bon travail!",
+				voirConjugaison: "Vous faites",
+			},
+
+			{
+				first: "Je ne sais pas comment elles",
+				reponse: "font",
+				last: "ça.",
+				voirConjugaison: "Ils/Elles font",
+			},
+		],
+	},
+	{
+		nombre: "S'assoir",
+		conjugaison: [
+			{
+				first: "Je m'",
+				reponse: "assois",
+				last: "",
+				voirConjugaison: "Je m'assois",
+			},
+			{
+				first: "Tu t'",
+				reponse: "assois",
+				last: "",
+				voirConjugaison: "Tu t'assois",
+			},
+			{
+				first: "Il s'",
+				reponse: "assoit",
+				last: "",
+				voirConjugaison: "Il/Elle s'assoit",
+			},
+			{
+				first: "On s'",
+				reponse: "assoit",
+				last: "",
+				voirConjugaison: "On s'assoit",
+			},
+			{
+				first: "Nous nouss",
+				reponse: "asseyons",
+				last: "",
+				voirConjugaison: "Nous nous asseyons",
+			},
+			{
+				first: "Vous vous",
+				reponse: "asseyez",
+				last: "",
+				voirConjugaison: "Vous vous asseyez",
+			},
+
+			{
+				first: "Ils s'",
+				reponse: "assoient",
+				last: "",
+				voirConjugaison: "Ils/Elles s'assoient",
+			},
+		],
+	},
+
+	{
+		nombre: "S'appeler",
+		conjugaison: [
+			{
+				first: "Je",
+				reponse: "m'appelle",
+				last: "Martin.",
+				voirConjugaison: "Je m'appelle",
+			},
+			{
+				first: "¿Tu",
+				reponse: "t'appelles",
+				last: "comment?",
+				voirConjugaison: "Tu t'appelles",
+			},
+			{
+				first: "Il",
+				reponse: "s'appelle",
+				last: "Bernard.",
+				voirConjugaison: "Il/Elle s'appelle",
+			},
+			{
+				first: "On",
+				reponse: "s'appelle",
+				last: '"Encadrements Bienert".',
+				voirConjugaison: "On s'appelle",
+			},
+			{
+				first: "Nous",
+				reponse: "nous appelons",
+				last: '"Les gars de Nantes".',
+				voirConjugaison: "Nous nous appelons",
+			},
+			{
+				first: "¿Vous",
+				reponse: "vous appelez",
+				last: '"Moignard"?',
+				voirConjugaison: "Vous vous appelez",
+			},
+
+			{
+				first: "Elles",
+				reponse: "s'appellent",
+				last: "Eva et Marie.",
+				voirConjugaison: "Ils/Elles s'appellent",
+			},
+		],
+	},
+
+	{
 		nombre: "Vouloir",
 		conjugaison: [
 			{
@@ -470,92 +1024,6 @@ export const data = [
 				reponse: "veulent",
 				last: "aborder.",
 				voirConjugaison: "Ils/Elles veulent",
-			},
-		],
-	},
-	{
-		nombre: "Aimer",
-		conjugaison: [
-			{
-				first: "J'",
-				reponse: "aime",
-				last: "",
-				voirConjugaison: "J'aime",
-			},
-			{ first: "Tu", reponse: "aimes", last: "", voirConjugaison: "Tu aimes" },
-			{
-				first: "Il",
-				reponse: "aime",
-				last: "",
-				voirConjugaison: "Il/Elle aime",
-			},
-			{ first: "On", reponse: "aime", last: "", voirConjugaison: "On aime" },
-			{
-				first: "Nous",
-				reponse: "aimons",
-				last: "",
-				voirConjugaison: "Nous aimons",
-			},
-			{
-				first: "Vous",
-				reponse: "aimez",
-				last: "",
-				voirConjugaison: "Vous aimez",
-			},
-
-			{
-				first: "Ils",
-				reponse: "aiment",
-				last: "",
-				voirConjugaison: "Ils/Elles aiment",
-			},
-		],
-	},
-	{
-		nombre: "Arrêter",
-		conjugaison: [
-			{
-				first: "J'",
-				reponse: "arrête",
-				last: "",
-				voirConjugaison: "Je arrête",
-			},
-			{
-				first: "Tu",
-				reponse: "arrêtes",
-				last: "",
-				voirConjugaison: "Tu arrêtes",
-			},
-			{
-				first: "Il",
-				reponse: "arrête",
-				last: "",
-				voirConjugaison: "Il/Elle arrête",
-			},
-			{
-				first: "On",
-				reponse: "arrête",
-				last: "",
-				voirConjugaison: "On arrête",
-			},
-			{
-				first: "Nous",
-				reponse: "arrêtons",
-				last: "",
-				voirConjugaison: "Nous arrêtons",
-			},
-			{
-				first: "Vous",
-				reponse: "arrêtez",
-				last: "",
-				voirConjugaison: "Vous arrêtez",
-			},
-
-			{
-				first: "Ils",
-				reponse: "arrêtent",
-				last: "",
-				voirConjugaison: "Ils/Elles arrêtent",
 			},
 		],
 	},
